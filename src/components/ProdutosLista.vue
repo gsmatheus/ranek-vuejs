bea961469fa4e3b283d537cd396aee2437e53d98
Matheus <etec.matheus@gmail.com>
2022-02-08 18:33:01 -0300

    Estilizado a view que mostra o resultados da pesquisa.

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,11 +1,20 @@
 <template>
   <section class="produtos_container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-      <p class="preco">{{ produto.preco }}</p>
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="">{{ produto.descricao }}</p>
-      {{ url }}
+    <!--  Verifica se tem produtos  -->
+    <div v-if="produtos && produtos.length > 0" class="produtos">
+      <!-- Percorre os produtos -->
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p class="">{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <!-- Não tem produtos -->
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
@@ -47,5 +56,43 @@ export default {
 </script>
 
 <style scoped>
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
 
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>
