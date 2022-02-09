4ac62bdf04d4584d32e5e57e344fab19f4e46441
Matheus <etec.matheus@gmail.com>
2022-02-09 15:54:00 -0300

    Cadastrando o usuario na api

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -4,7 +4,7 @@
     <transition mode="out-in">
       <button v-if="!criar" @click="criar = true" class="btn">Criar Conta</button>
       <UsuarioForm v-else>
-        <button class="btn btn-form">Criar Usuário</button>
+        <button class="btn btn-form" @click.prevent="criarUsuario">Criar Usuário</button>
       </UsuarioForm>
     </transition>
   </section>
@@ -21,6 +21,17 @@ export default {
   data() {
     return {
       criar: false
+    }
+  },
+  methods: {
+    async criarUsuario() {
+      try {
+        await this.$store.dispatch('criarUsuario', this.$store.state.usuario)
+        await this.$store.dispatch('getUsuario', this.$store.state.usuario.email)
+        await this.$router.push({name: 'usuario'})
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 
